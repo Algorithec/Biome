1b927eb3b7a95b701bbd5266b1d48d8acdb2aed8
Raghav Dabra <raghavdabra@gmail.com>
2026-05-16 15:22:38 +1000

    feat(payments-hs): Webhook.hs

---
+++ payments-hs/src/Payments/Webhook.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Payments.Webhook
+  ( verifyCashfreeSignature,
+  )
+where
+
+import Crypto.Hash.Algorithms (SHA256)
+import Crypto.MAC.HMAC (HMAC, hmac, hmacGetDigest)
+import Crypto.Util (constTimeEq)
+import Data.ByteArray (convert)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Base64 as B64
+import qualified Data.CaseInsensitive as CI
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import Network.HTTP.Types (RequestHeaders)
+
+verifyCashfreeSignature :: BS.ByteString -> RequestHeaders -> T.Text -> Bool
+verifyCashfreeSignature rawBody headers secret =
+  case (lookupHeader "x-webhook-timestamp", lookupHeader "x-webhook-signature") of
+    (Just ts, Just sig) ->
+      let signStr = BS.concat [ts, rawBody]
+          key = TE.encodeUtf8 secret
+          digest :: HMAC SHA256
+          digest = hmac key signStr
+          computed = B64.encode (convert (hmacGetDigest digest) :: BS.ByteString)
+       in constTimeEq computed sig
+    _ -> False
+  where
+    lookupHeader :: CI.CI BS.ByteString -> Maybe BS.ByteString
+    lookupHeader k = lookup k headers
